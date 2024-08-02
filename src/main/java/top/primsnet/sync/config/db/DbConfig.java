78f37bd00aef488ec54052a96c0fcbac7afe1394
贾乾 <jiaqian0051@163.com>
2024-08-02 16:27:13 +0800

    更新数据源连接池

--- src/main/java/top/primsnet/sync/config/db/DbConfig.java
+++ src/main/java/top/primsnet/sync/config/db/DbConfig.java
@@ -1,16 +1,16 @@
 package top.primsnet.sync.config.db;
 
-import com.zaxxer.hikari.HikariDataSource;
 import org.noear.solon.annotation.Bean;
 import org.noear.solon.annotation.Configuration;
 import org.noear.solon.annotation.Inject;
+import org.noear.solon.data.dynamicds.DynamicDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DbConfig {
     @Bean(value = "db1", typed = true)
-    public DataSource db1(@Inject("${db.db1}") HikariDataSource ds) {
+    public DataSource db1(@Inject("${db.db1}") DynamicDataSource ds) {
         return ds;
     }
 
