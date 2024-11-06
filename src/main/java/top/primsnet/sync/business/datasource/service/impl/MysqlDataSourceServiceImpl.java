a588debe8edd7842e28bb53c1a5e35724a745e55
贾乾 <jiaqian0051@163.com>
2024-11-06 11:11:17 +0800

    获取数据源字段

--- src/main/java/top/primsnet/sync/business/datasource/service/impl/MysqlDataSourceServiceImpl.java
+++ src/main/java/top/primsnet/sync/business/datasource/service/impl/MysqlDataSourceServiceImpl.java
@@ -23,6 +23,10 @@ import top.primsnet.sync.common.exception.ServiceException;
 
 import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 /**
@@ -50,17 +54,17 @@ public class MysqlDataSourceServiceImpl implements DataSourceService {
     public String createDataSourceStr(CreateDataSourceStrTO dataSourceStrTO) {
         //参数校验
         if (ObjUtil.hasEmpty(dataSourceStrTO.getBeanName(), dataSourceStrTO.getPassword(), dataSourceStrTO.getType(),
-                dataSourceStrTO.getUrl(), dataSourceStrTO.getUserName())){
+                dataSourceStrTO.getUrl(), dataSourceStrTO.getUserName())) {
             throw new ServiceException("创建数据源动态加载json字符串失败，参数校验失败");
         }
         //构建动态加载数据源json串
         JSONObject configJson = new JSONObject();
         configJson.set("driverClassName", SysCommonEnum.MYSQL_8_DRIVER_CLASS_NAME.getValue());
-        configJson.set("type",SysCommonEnum.DATA_SOURCE_POOL_TYPE.getValue());
-        configJson.set("strict",SysCommonEnum.DATA_SOURCE_STRICT_CLOSE_TYPE.getValue());
-        configJson.set("jdbcUrl",dataSourceStrTO.getUrl());
-        configJson.set("username",dataSourceStrTO.getUserName());
-        configJson.set("password",dataSourceStrTO.getPassword());
+        configJson.set("type", SysCommonEnum.DATA_SOURCE_POOL_TYPE.getValue());
+        configJson.set("strict", SysCommonEnum.DATA_SOURCE_STRICT_CLOSE_TYPE.getValue());
+        configJson.set("jdbcUrl", dataSourceStrTO.getUrl());
+        configJson.set("username", dataSourceStrTO.getUserName());
+        configJson.set("password", dataSourceStrTO.getPassword());
         return JSONUtil.toJsonStr(configJson);
     }
 
@@ -73,29 +77,29 @@ public class MysqlDataSourceServiceImpl implements DataSourceService {
      */
     @Override
     public void initDataSource(String beanName, String configJsonStr) {
-        if (StrUtil.hasEmpty(beanName,configJsonStr)){
+        if (StrUtil.hasEmpty(beanName, configJsonStr)) {
             throw new ServiceException("加载mysql数据源失败,参数校验失败-参数不完整");
         }
         //json格式校验
-        if (!JSONUtil.isTypeJSON(configJsonStr)){
+        if (!JSONUtil.isTypeJSON(configJsonStr)) {
             throw new ServiceException("当前数据源配置json字符串格式错误,请检查数据源配置或重新配置数据源");
         }
         try {
-            log.info("开始加载mysql数据源,名称:{},配置:{}",beanName,configJsonStr);
+            log.info("开始加载mysql数据源,名称:{},配置:{}", beanName, configJsonStr);
             Props props = new Props();
             props.loadAdd(Utils.buildProperties(configJsonStr));
             DataSource ds = props.toBean(HikariDataSource.class);
             dds.addTargetDataSource(beanName, ds);
             //获取数据源
             DataSource dataSource = dds.getTargetDataSource(beanName);
-            log.info("加载数据源后获取数据源成功,bean名称:{},数据源:{}",beanName,dataSource);
-            if (ObjUtil.isEmpty(dataSource)){
+            log.info("加载数据源后获取数据源成功,bean名称:{},数据源:{}", beanName, dataSource);
+            if (ObjUtil.isEmpty(dataSource)) {
                 throw new ServiceException("加载mysql数据源失败,名称：%s,数据源添加后未获取到".formatted(beanName));
             }
-            log.info("加载mysql数据源成功,名称：{}",beanName);
+            log.info("加载mysql数据源成功,名称：{}", beanName);
         } catch (Exception e) {
-            log.error("加载mysql数据源失败，名称:{},配置:{},异常:{}",beanName,configJsonStr, ExceptionUtil.stacktraceToString(e));
-            throw new ServiceException("加载mysql数据源失败,名称：%s,异常：%s".formatted(beanName,ExceptionUtil.stacktraceToString(e)));
+            log.error("加载mysql数据源失败，名称:{},配置:{},异常:{}", beanName, configJsonStr, ExceptionUtil.stacktraceToString(e));
+            throw new ServiceException("加载mysql数据源失败,名称：%s,异常：%s".formatted(beanName, ExceptionUtil.stacktraceToString(e)));
         }
     }
 
@@ -106,47 +110,81 @@ public class MysqlDataSourceServiceImpl implements DataSourceService {
      */
     @Override
     public void logoutDataSource(String beanName) {
-        if (StrUtil.isEmpty(beanName)){
+        if (StrUtil.isEmpty(beanName)) {
             throw new ServiceException("卸载mysql数据源失败,参数校验失败-参数不完整");
         }
         //获取数据源
         DataSource dataSource = dds.getTargetDataSource(beanName);
-        if (ObjUtil.isEmpty(dataSource)){
+        if (ObjUtil.isEmpty(dataSource)) {
             log.warn("卸载mysql数据源失败,名称：%s,数据源不存在,请确认服务是否正常".formatted(beanName));
 //            throw new ServiceException("卸载mysql数据源失败,名称：%s,数据源不存在".formatted(beanName));
         }
         //卸载数据源
         try {
             dds.removeTargetDataSource(beanName);
-            log.info("卸载mysql数据源成功,名称：{}",beanName);
+            log.info("卸载mysql数据源成功,名称：{}", beanName);
         } catch (IOException e) {
             e.printStackTrace();
-            throw new ServiceException("卸载数据源失败,bean名称：%s,异常:%s".formatted(beanName,ExceptionUtil.stacktraceToString(e)));
+            throw new ServiceException("卸载数据源失败,bean名称：%s,异常:%s".formatted(beanName, ExceptionUtil.stacktraceToString(e)));
         }
     }
 
     /**
      * 获取数据源字段
-     * @param beanName 数据源
+     *
+     * @param beanName  数据源
      * @param tableName 表明
      * @return 字段集合
      */
     @Override
-    public List<GetDataSourceFieldsTO> getFields(String beanName,String tableName) {
-        if (StrUtil.isEmpty(beanName)){
+    public List<GetDataSourceFieldsTO> getFields(String beanName, String tableName) {
+        if (StrUtil.isEmpty(beanName)) {
             throw new ServiceException("获取数据源字段mysql数据源失败,参数校验失败-参数不完整");
         }
         //获取数据源
         DataSource dataSource = dds.getTargetDataSource(beanName);
-        if (ObjUtil.isEmpty(dataSource)){
+        if (ObjUtil.isEmpty(dataSource)) {
             throw new ServiceException("获取数据源字段mysql数据源失败,名称：%s,数据源不存在".formatted(beanName));
         }
-        String schema = null;
+        Connection connection =null;
+        Statement statement =null;
         try {
             DynamicDsKey.setCurrent(beanName);
-            return mysqlDataSourceMapper.getFields("sss",tableName);
+            connection = dataSource.getConnection();
+            statement = connection.createStatement();
+            String sql = "select column_name as fieldName,column_comment as fieldAnnotation,data_type as fieldType\n" +
+                    " from information_schema.columns where table_schema = 'sss' and  table_name = 'test'";
+            boolean execute = statement.execute(sql);
+            if (execute) {
+                ResultSet resultSet = statement.getResultSet();
+                while (resultSet.next()) {
+                    //todo 需要提取工具类
+                    //todo 组装结果
+                    System.out.println(resultSet.getString("fieldName") + "-"
+                            + resultSet.getString("fieldAnnotation") + "-" + resultSet.getString("fieldType"));
+                }
+            }
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            log.error("执行获取mysql字段异常,表:{},异常:{}", tableName, ExceptionUtil.stacktraceToString(e));
         } finally {
+            if (ObjUtil.isNotNull(statement)){
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    log.error("关闭statement异常:{}",ExceptionUtil.stacktraceToString(e));
+                }
+            }
+            if (ObjUtil.isNotNull(connection)){
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    log.error("关闭connection异常:{}",ExceptionUtil.stacktraceToString(e));
+                }
+            }
             DynamicDsKey.setCurrent("db1");
         }
+        return null;
     }
 }
---
+++ src/main/java/top/primsnet/sync/business/datasource/utils/SqlExecuteUtil.java
@@ -0,0 +1,13 @@
+package top.primsnet.sync.business.datasource.utils;
+
+/**
+ * Author:  joshua
+ * Description: TODO
+ * DateTime: 2024/11/6 11:07
+ **/
+public class SqlExecuteUtil {
+
+
+
+
+}
