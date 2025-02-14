5902ddb5111171672e036fea7e9079b85f35b97a
Joshua <jq@1.com>
2025-02-14 15:31:34 +0800

    前端更新

--- src/main/java/top/prism/sync/DataPulseApp.java
+++ src/main/java/top/prism/sync/DataPulseApp.java
@@ -1,11 +1,14 @@
 package top.prism.sync;
 
+import lombok.extern.slf4j.Slf4j;
 import org.noear.solon.Solon;
 import org.noear.solon.annotation.SolonMain;
 
 @SolonMain
+@Slf4j
 public class DataPulseApp {
     public static void main(String[] args) {
         Solon.start(DataPulseApp.class, args);
+        log.info("您可通过 http://ip:端口 访问项目自带后台配置");
     }
 }
---
+++ src/main/resources/static/assets/castArray-DBWPTtuz.js
@@ -0,0 +1,1 @@
+import{bv as a}from"./index-Cwx_NiLh.js";function e(){if(!arguments.length)return[];var r=arguments[0];return a(r)?r:[r]}export{e as c};
